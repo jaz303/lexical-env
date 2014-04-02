77e31971bd584d3f6f06066e5116d4b8c1ba4bd2
Jason Frame <jason@onehackoranother.com>
2014-04-02 19:59:30 +0100

    initial

---
+++ index.js
@@ -0,0 +1,38 @@
+exports.create  = create;
+exports.find    = find;
+exports.def     = def;
+exports.get     = get;
+exports.set     = set;
+
+var hop = Object.hasOwnProperty,
+    gpo = Object.getPrototypeOf;
+
+function create(parent) {
+    return Object.create(parent || null);
+}
+
+function find(env, key) {
+    while (env) {
+        if (hop(env, key)) {
+            return env;
+        } else {
+            env = gpo(env);
+        }
+    }
+    throw new Error("unknown environment key: " + key);
+}
+
+function def(env, key, value) {
+    if (hop(env, key)) {
+        throw new Error("cannot redefine key: " + key);
+    }
+    env[key] = value;
+}
+
+function get(env, key) {
+    return find(env, key)[key];
+}
+
+function set(env, key, value) {
+    find(env, key)[key] = value;
+}
---
+++ test/all.js
@@ -0,0 +1,37 @@
+var test    = require('tape'),
+    env     = require('..');
+
+test('create', function(a) {
+
+
+    a.end();
+});
+
+test('create child', function(a) {
+
+    a.end();
+});
+
+test('find', function(a) {
+
+
+    a.end();
+});
+
+test('def', function(a) {
+
+
+    a.end();
+});
+
+test('get', function(a) {
+
+
+    a.end();
+});
+
+test('set', function(a) {
+
+
+    a.end();
+});
